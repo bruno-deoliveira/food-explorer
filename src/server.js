d7e44ca63941d3dc2151a3bebc99c0a418f104c2
Bruno <brunoalmeida0805@gmail.com>
2023-09-04 22:00:27 -0300

    add details

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require ("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require ("express");     //aqui estou importando para usar tudo que esá na pasta node_modules
 const routes = require("./routes");
 
 migrationsRun();
 
 const app = express();                   //para iniciar o express 
+app.use(cors());
 app.use(express.json());                 // para receber as informçãoes atraves do corpo da requisição usando json
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
@@ -33,5 +35,5 @@ app.use((error, request, response, next) => {
 });
 
 
-const PORT = 3333                        // Dizemos qual o endereço que ele vai atender as solicitações 
+const PORT = process.env.PORT || 3333;                       // Dizemos qual o endereço que ele vai atender as solicitações 
 app.listen(PORT, () => console.log(`Server us running on Port ${PORT}`));
